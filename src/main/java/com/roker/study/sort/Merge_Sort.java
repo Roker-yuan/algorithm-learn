afaf7a4733f60822c2fc0991edbc0223d1dcf54a
Roker <lpy962464@163.com>
2021-02-03 17:25:28 +0800

    工具类优化，新增交换，比较等方法

---
+++ src/main/java/com/roker/study/sort/Merge_Sort.java
@@ -0,0 +1,11 @@
+package com.roker.study.sort;
+
+/**
+ * @Author Roker
+ * @Date 2021/02/03 17:18
+ * @Title Merge_Sort
+ * @Description 归并排序（Merge Sort）
+ */
+
+public class Merge_Sort {
+}
--- src/main/java/com/roker/study/utils/ArrayUtil.java
+++ src/main/java/com/roker/study/utils/ArrayUtil.java
@@ -8,6 +8,12 @@ package com.roker.study.utils;
  */
 
 public class ArrayUtil {
+
+
+    /**
+     * 输出数组
+     * @param arr
+     */
     public static void print(int[] arr){
         StringBuilder sb = new StringBuilder();
         sb.append("[");
@@ -21,4 +27,40 @@ public class ArrayUtil {
         }
         System.out.println("结果打印：" + sb);
     }
+
+    /**
+     * 数组索引为a,b位置的值交换位置
+     * @param arr
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int[] swap(int[] arr, int a, int b){
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+        return arr;
+    }
+
+    /**
+     * 比较数组arr中索引i1和i2对应值大小，返回其差值
+     * @param arr
+     * @param i1
+     * @param i2
+     * @return
+     */
+    public static int cmp(int[] arr, int i1, int i2) {
+        return (arr[i1] - arr[i2]) ;
+    }
+
+    /**
+     * 比较元素v1和v2对应值大小，返回其差值
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static int cmp(int v1, int v2) {
+        return (v1 - v2);
+    }
+
 }
